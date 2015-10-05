5e818dad843a147fd10bc802c852a3ae1d36696f
Winjay <winjaylu@163.com>
2015-10-05 13:55:39 +0800

    初始化项目

---
+++ simuyun/simuyun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  simuyun
+//
+//  Created by Luwinjay on 15/10/5.
+//  Copyright © 2015年 YTWealth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
